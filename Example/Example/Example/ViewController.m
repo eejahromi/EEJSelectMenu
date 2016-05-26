0444b8b7a7460463c34f1867110737f436dce0fd
Ehsan Jahromi <ehsan.enj@gmail.com>
2016-05-25 23:51:22 -0700

    example finished

--- Example/Example/Example/ViewController.m
+++ Example/Example/Example/ViewController.m
@@ -29,6 +29,49 @@
     [self presentViewController:menu animated:NO completion:nil];
 }
 
+- (IBAction)fourButtonsLeftPressed:(UIButton *)sender {
+    EEJSelectMenu *menu = [[EEJSelectMenu alloc]init];
+    menu.buttonNames = @[@"COFFEE",@"TEA",@"WATER",@"SODA"];
+    menu.animationStyle = EJAnimationStyleMoveInFromLeft;
+    menu.delegate = self;
+    menu.selectedButtonColor = [UIColor orangeColor];
+    [self presentViewController:menu animated:NO completion:nil];
+}
+
+- (IBAction)fiveButtonsRightPressed:(UIButton *)sender {
+    EEJSelectMenu *menu = [[EEJSelectMenu alloc]initWithButtons:@[@"MERCURY",@"JUPITER",@"EARTH",@"MARS",@"VENUS"]
+                                                 animationStyle:EJAnimationStyleMoveInFromRight
+                                                    andDelegate:self];
+    menu.selectedButtonColor = [UIColor lightGrayColor];
+    [self presentViewController:menu animated:NO completion:nil];
+}
+
+- (IBAction)fiveButtonsWidenPressed:(UIButton *)sender {
+    EEJSelectMenu *menu = [[EEJSelectMenu alloc]initWithButtons:@[@"LEBRON",@"KOBE",@"K.D",@"STEPH",@"RUSSELL"]
+                                                 animationStyle:EJAnimationStyleWiden
+                                                    andDelegate:self];
+    menu.selectedButtonColor = [UIColor grayColor];
+    [self presentViewController:menu animated:NO completion:nil];
+}
+
+- (IBAction)sixButtonScalePressed:(UIButton *)sender {
+    EEJSelectMenu *menu = [[EEJSelectMenu alloc]init];
+    menu.buttonNames = @[@"COKE",@"SPRITE",@"FANTA",@"PEPSI",@"MOUNTAIN DEW",@"DR.PEPPER"];
+    menu.animationStyle = EJAnimationStyleScale;
+    menu.delegate = self;
+    menu.selectedButtonColor = [UIColor yellowColor];
+    [self presentViewController:menu animated:NO completion:nil];
+}
+
+- (IBAction)sixButtonsAlternatePressed:(UIButton *)sender {
+    EEJSelectMenu *menu = [[EEJSelectMenu alloc]init];
+    menu.buttonNames = @[@"PIZZA",@"BURGER",@"SOUP",@"SALAD",@"STEAK",@"LOBSTER"];
+    menu.animationStyle = EJAnimationStyleAlternate;
+    menu.delegate = self;
+    menu.selectedButtonColor = [UIColor blackColor];
+    [self presentViewController:menu animated:NO completion:nil];
+}
+
 #pragma mark - EJSelectMenuDelegate
 
 - (void)EJSelectMenuButtonWasPressedWithTitle:(NSString *)title {
